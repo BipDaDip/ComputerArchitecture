183ccf6cf48ee7f09022125969df49fccb59152a
BipDaDip <52990308+BipDaDip@users.noreply.github.com>
2021-10-24 22:31:25 +0800

    hw1 v1

---
+++ hw1/lab1.s
@@ -0,0 +1,33 @@
+.data
+in: .word 6
+
+.text
+main:
+
+lw   a0, in
+jal  a1,  isPerfect
+mv   a0, a1
+
+ecall
+
+isPerfect:
+#function a0: input, a1: ret
+
+srli a1, a0, 1   # a2 = a0 >> 1
+li   a3, 0       # a3 = 0 (use as temporary sum)
+LOOP:            # do
+
+rem  a2, a0, a1  # a2 = a0 % a1
+
+bnez a2, EIF     # if a2 == 0
+add  a3, a3, a1  # a3 += a1
+EIF:             # endif
+addi a1, a1, -1  # a1--
+bnez a1, LOOP    # while (a2 != 0)
+
+li   a1, 0       # a0 = (a3 == a0) ? 1 : 0;
+bne  a3, a0, END
+li   a1, 1
+
+END:
+ret
