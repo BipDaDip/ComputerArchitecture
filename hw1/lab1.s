bfa26c3bd0e77060fc61b070ff3ed34c9de2fe4d
BipDaDip <52990308+BipDaDip@users.noreply.github.com>
2021-10-24 22:45:14 +0800

    hw1 - upload correct version

--- hw1/lab1.s
+++ hw1/lab1.s
@@ -1,17 +1,19 @@
 .data
-in: .word 6
+in: .word 6     # test input
 
 .text
 main:
 
 lw   a0, in
-jal  a1,  isPerfect
-mv   a0, a1
+jal  ra, isPerfect
 
+mv   a0, a1      # output answer
+li a7, 1
+ecall
+li   a7, 10      # end program
 ecall
 
-isPerfect:
-#function a0: input, a1: ret
+isPerfect:       #function a0: input, a1: ret
 
 srli a1, a0, 1   # a2 = a0 >> 1
 li   a3, 0       # a3 = 0 (use as temporary sum)
